52c7537ac598ecf30c875803a0401a219c582dfe
Rassim Medkour <racimmedkour@gmail.com>
2022-09-28 13:39:28 +0100

    added card list and cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState,useEffect } from "react";
+import { CardList } from './Components/card-list/card-list.js';
 function App() {
+/*   { name: 'Frankenstien', id: '1a' }, { name: 'Dracula', id: '2a' }, { name: 'Zombie', id: '3a' }
+ */  const [monsters,setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then(users => setMonsters(users));
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <CardList monsters ={monsters}>
+      </CardList>
+      
+
+
     </div>
   );
 }
