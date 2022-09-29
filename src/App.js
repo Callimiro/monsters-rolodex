32b4e079dcc8624fd443d29ded5317cc7ac390d8
Rassim Medkour <racimmedkour@gmail.com>
2022-09-29 01:08:57 +0100

    added searchBox component and more styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import React, { useState,useEffect } from "react";
 import { CardList } from './Components/card-list/card-list.js';
+import {SearchBox } from './Components/search-box/search-box.js'; 
 function App() {
 /*   { name: 'Frankenstien', id: '1a' }, { name: 'Dracula', id: '2a' }, { name: 'Zombie', id: '3a' }
  */  
 
 const [monsters,setMonsters] = useState([]);
 const [searchField,setSearchField] = useState('');
-
+  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
@@ -17,8 +18,8 @@ const [searchField,setSearchField] = useState('');
   
   return (
     <div className="App">
-      
-      <input type='text' placeholder='search monster' onChange={e => setSearchField(e.target.value)}></input>
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder='search monster' handleChange={e => setSearchField(e.target.value)}></SearchBox>
       <CardList monsters ={monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))}>
       </CardList>
       
